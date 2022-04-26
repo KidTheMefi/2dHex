28f2393997e37be7fd95ba2be36023200d78f6ce
Petro Zhakun <p.zhakun@gmail.com>
2022-04-26 23:18:01 +0300

    added HexMouseController & PlayerGroupSelection

---
+++ Assets/Scripts/PlayerGroup/PlayerSelectControl.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Zenject;
+
+public class PlayerSelectControl : IInitializable, IDisposable
+{
+    private PlayerGroupModel _playerGroupModel;
+    private PlayerGroupView _playerGroupView;
+    private HexMouseController _hexMouseController;
+    
+    public PlayerSelectControl(PlayerGroupModel playerGroupModel, PlayerGroupView playerGroupView, HexMouseController hexMouseController)
+    {
+        _playerGroupModel = playerGroupModel;
+        _playerGroupView = playerGroupView;
+        _hexMouseController = hexMouseController;
+    }
+    public void Initialize()
+    {
+        _hexMouseController.HighlightedHexClicked += Selected;
+    }
+
+    private void Selected(Vector2Int axial)
+    {
+        if ( axial == _playerGroupModel.AxialPosition && !_playerGroupModel.IsSelected )
+        {
+            _playerGroupModel.Select(true);
+            _playerGroupView.ShowSelect(true);
+        }
+        else if (axial != _playerGroupModel.AxialPosition)
+        {
+            _playerGroupModel.Select(false);
+            _playerGroupView.ShowSelect(false);
+        }
+    }
+    
+    public void Dispose()
+    {
+        _hexMouseController.HighlightedHexClicked -= Selected;
+    }
+}
